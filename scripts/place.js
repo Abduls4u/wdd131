9c9912ed754083de2bfb695f60d963b8d1f48439
Abdulsomad <abdulsalamabdulsomad14@gmail.com>
2025-09-20 09:26:22 +0100

    Add solution to Wk03 tasks

--- scripts/place.js
+++ scripts/place.js
@@ -2,12 +2,7 @@ document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
 
 function calculateWindChill(temp, wind) {
-  return (
-    13.12 +
-    0.6215 * temp -
-    11.37 * Math.pow(wind, 0.16) +
-    0.3965 * temp * Math.pow(wind, 0.16)
-  ).toFixed(1);
+  return (13.12 + 0.6215 * temp - 11.37 * Math.pow(wind, 0.16) + 0.3965 * temp * Math.pow(wind, 0.16)).toFixed(1);
 }
 
 const temp = parseFloat(document.getElementById("temp").textContent);
