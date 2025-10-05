b7b81d6fbfe181fda10f6d863d1f8c2003569ee5
Abdulsomad <abdulsalamabdulsomad14@gmail.com>
2025-10-05 11:25:44 +0100

    add solution to tasks

--- scripts/form.js
+++ scripts/form.js
@@ -1,11 +1,3 @@
-// // Product data array
-// const products = [
-//   { id: "p1", name: "Super Blender 3000" },
-//   { id: "p2", name: "Smart Toaster Pro" },
-//   { id: "p3", name: "Eco Kettle" },
-//   { id: "p4", name: "Air Purifier Max" },
-//   { id: "p5", name: "Coffee Maker Deluxe" }
-// ];
 
 const products = [
   {
